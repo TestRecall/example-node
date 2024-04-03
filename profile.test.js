421c680604a2fd443d7fde2711784d4bd53b59dd
Andrew Klotz <agc.klotz@gmail.com>
2024-04-03 14:18:22 +0000

    check failures

--- profile.test.js
+++ profile.test.js
@@ -1,3 +1,3 @@
 const generate =  require('./utils/generate');
 
-generate({ name: "profile", tests: 10, failRate: 0.01, skipRate: 0, missChance: 0 })
+generate({ name: "profile", tests: 10, failRate: 0.51, skipRate: 0, missChance: 0 })
